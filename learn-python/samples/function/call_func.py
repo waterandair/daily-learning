f29a8c52ccd3904c4dea88bb839c9ab2f13d189c
zj <water.air.zj@gmail.com>
2018-04-10 06:05:34 +0800

    learn-python

--- learn-python/samples/function/call_func.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-x = abs(100)
-y = abs(-20)
-print(x, y)
-print('max(1, 2, 3_num_date_time) =', max(1, 2, 3))
-print('min(1, 2, 3_num_date_time) =', min(1, 2, 3))
-print('sum([1, 2, 3_num_date_time]) =', sum([1, 2, 3]))
