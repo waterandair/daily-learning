1e3c3150f2eb598be934be82697a6feb57e21021
zj <water.air.zj@gmail.com>
2018-06-04 06:55:16 +0800

    鸢尾花分类-逻辑回归

--- learn-ml/regression/logistic_iris.py
+++ learn-ml/regression/logistic_iris.py
@@ -29,23 +29,24 @@ if __name__ == '__main__':
         ('clf', LogisticRegression())]  # 逻辑回归模型
     )
 
-    lr.fit(x, y.ravel())
-
+    # 为了可视化，仅使用前两列特征
     x = x[:, :2]
+    lr.fit(x, y.ravel())  # 训练模型,计算参数
+
     # 画图查看结果, 构建一个由250000个点组成的平面
     N, M = 500, 500  # 横纵各采样多少个值
     x1_min, x1_max = x[:, 0].min(), x[:, 0].max()  # 第0列的范围
     x2_min, x2_max = x[:, 1].min(), x[:, 1].max()  # 第1列的范围
-    t1 = np.linspace(x1_min, x1_max, N)
+    t1 = np.linspace(x1_min, x1_max, N)  # 等差数列
     t2 = np.linspace(x2_min, x2_max, M)
     x1, x2 = np.meshgrid(t1, t2)  # 生成网格采样点
 
     x_test = np.stack((x1.flat, x2.flat), axis=1)  # 250000个测试点
+    y_hat = lr.predict(x_test)  # 预测值
+    y_hat = y_hat.reshape(x1.shape)  # 使之与输入的形状相同 (250000, 1)
 
     cm_light = colors.ListedColormap(['#77E0A0', '#FF8080', '#A0A0FF'])
     cm_dark = colors.ListedColormap(['g', 'r', 'b'])
-    y_hat = lr.predict(x_test)  # 预测值
-    y_hat = y_hat.reshape(x1.shape)  # 使之与输入的形状相同
     plt.pcolormesh(x1, x2, y_hat, cmap=cm_light)  # 预测值的显示
     plt.scatter(x[:, 0], x[:, 1], s=10, c=y.ravel(), cmap=cm_dark)  # 样本点的显示
     plt.xlabel('petal length')
@@ -59,6 +60,7 @@ if __name__ == '__main__':
     y_hat = lr.predict(x)
     y = y.reshape(-1)
     result = y_hat == y
+    print(result)
     acc = np.mean(result)
     print('准确度: {}'.format(100 * acc))
 
