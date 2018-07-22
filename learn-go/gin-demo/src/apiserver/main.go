261a72be374a41661f794ce5de16a0c0ff629c8b
zj <water.air.zj@gmail.com>
2018-07-23 07:07:10 +0800

    gin 管理配置

--- learn-go/gin-demo/src/apiserver/main.go
+++ learn-go/gin-demo/src/apiserver/main.go
@@ -3,21 +3,36 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"apiserver/router"
+	"apiserver/config"
 	"log"
 	"net/http"
 	"time"
 	"errors"
+	"github.com/spf13/viper"
+	"github.com/spf13/pflag"
 )
 
 var (
-	port = ":8080"
+	cfg = pflag.StringP("config", "c", "", "apiserver config file path")  // 命令行参数
 )
 
 func main() {
+	pflag.Parse()
+
+	// 初始化配置
+	if err := config.Init(*cfg); err != nil {
+		panic(err)
+	}
+
+	// 设置 gin 模式
+	gin.SetMode(viper.GetString("runmode"))
+
 	// 初始化 gin
 	g := gin.New()
+
 	// gin middlewares
 	middlewares := []gin.HandlerFunc{}
+
 	// 路由
 	router.Load(g, middlewares...)
 
@@ -28,9 +43,10 @@ func main() {
 		}
 		log.Print("The router has been deployed successfully.")
 	}()
+
 	// 启动
-	log.Printf("Start to listening the incoming requests on http address: %s", ":8080")
-	log.Printf(http.ListenAndServe(port, g).Error())
+	log.Printf("Start to listening the incoming requests on http address: %s", viper.GetString("addr"))
+	log.Printf(http.ListenAndServe(viper.GetString("addr"), g).Error())
 }
 
 /*
@@ -39,8 +55,8 @@ func main() {
 	每隔一秒请求一次服务器,如果有 10 次失败,就返回 error,并终止程序
 */
 func pingServer() error {
-	for i :=0; i < 10; i++ {
-		resp, err := http.Get("http://127.0.0.1" + port + "/sd/health")
+	for i :=0; i < viper.GetInt("max_ping_count"); i++ {
+		resp, err := http.Get(viper.GetString("url") + "/sd/health")
 		if err == nil && resp.StatusCode == 200 {
 			return nil
 		}
